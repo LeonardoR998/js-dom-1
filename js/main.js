6a18617af4e09c55d84b69455ad5e45a1ff6f735
LeonardoR998 <leonardoricci839@gmail.com>
2024-10-22 14:38:32 +0200

    fix html tag and add function in js

--- js/main.js
+++ js/main.js
@@ -1,2 +1,23 @@
-const lamp = document.getElementById("lamp");
-const button = document.getElementById("button-lamp");
+const lampadina = document.getElementById("lampadina");
+const toggleButton = document.getElementById("toggleButton");
+
+// Stato iniziale della lampadina
+let lampadinaAccesa = false;
+
+// Aggiungi un evento al bottone per il click
+toggleButton.addEventListener("click", () => {
+  // SE la lampadina è spenta
+  if (lampadinaAccesa) {
+    // Spengo la lampadina
+    lampadina.src = "img/white_lamp.png";
+    toggleButton.textContent = "Accendi";
+    // ALTRIMENTI
+  } else {
+    // Accendo la lampadina
+    lampadina.src = "img/yellow_lamp.png";
+    toggleButton.textContent = "Spegni";
+  }
+
+  // Inverto lo stato della lampadina
+  lampadinaAccesa = !lampadinaAccesa;
+});
